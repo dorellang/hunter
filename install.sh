839b9b4133a6ad36575458474c25515daddce06b
Diego Orellana <dorellang@gmail.com>
2018-08-24 18:09:12 -0300

    updating gitignore and install

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -ex
 
 
 # download esprima
-curl -O https://raw.githubusercontent.com/dorellang/esprima-bin-wrapper/master/dist/esprima-bin-wrapper.node.js
+# curl -O https://raw.githubusercontent.com/dorellang/esprima-bin-wrapper/master/dist/esprima-bin-wrapper.node.js
 
 # download image and vm
 curl get.pharo.org/70+vm | bash
