c6c4735af40f3799d068b12b2bdfde99ad3b0028
Diego Orellana <dorellang@gmail.com>
2018-08-25 20:56:56 -0300

    updating install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@
 # We want to see what is executed and fail on bad exit codes
 set -ex
 
-
 # download image and vm
 curl get.pharo.org/70+vm | bash
 
 REPO="filetree://src"
 ./pharo Pharo.image metacello install $REPO BaselineOfHunter --install=development
+./pharo Pharo.image eval --save 'HNUtil baseDirectory: FileSystem workingDirectory. (IceRepositoryCreator new repository: nil; location: FileSystem workingDirectory; createRepository) register'
+
