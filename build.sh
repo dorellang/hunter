7ff14c1d90613128a74a13d9d6dfa2ac3c01151d
Martín Dias <tinchodias@gmail.com>
2019-07-29 19:48:06 -0400

    Trying to improve build script feedback

--- build.sh
+++ build.sh
@@ -3,17 +3,49 @@
 # We want to see what is executed and fail on bad exit codes
 set -ex
 
-# download image and vm
+echo ""
+echo " -------------------"
+echo "| Downloading Pharo |"
+echo " -------------------"
+echo ""
+
 curl https://get.pharo.org/64/70+vm | bash
+
+echo ""
+echo " ---------------------------------"
+echo "| Loading Hunter and dependencies |"
+echo " ---------------------------------"
+echo ""
 
 ./pharo Pharo.image metacello install "filetree://src" BaselineOfHunter --install=development
 ./pharo Pharo.image eval --save '(IceRepositoryCreator new repository: nil; location: FileSystem workingDirectory; createRepository) register'
 
+
+
 # if the --dev command-line flag was set, stop here.
 if [[ $* == *--dev* ]]
-then echo "Ready for development"; exit 0
-else echo "Deploying Hunter as standalone application..."
+then
+    echo ""
+    echo " -------------------------"
+    echo "| Build Finished: SUCCESS |"
+    echo " -------------------------"
+    echo ""
+    echo "Run './pharo-ui Pharo.image' to open Pharo with Hunter loaded."
+    exit 0
 fi
 
+echo ""
+echo " --------------------------------------------"
+echo "| Deploying Hunter as standalone application |"
+echo " --------------------------------------------"
+echo ""
+
 ./pharo Pharo.image eval --save "HNDeploymentScript deploy"
-echo "Ready. Open Hunter with start.sh script."
+
+echo ""
+echo " -------------------------"
+echo "| Build Finished: SUCCESS |"
+echo " -------------------------"
+echo ""
+
+echo "Run './start.sh' to open Hunter."
